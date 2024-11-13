79473d3ff293bbcc684cb49e0a8c8163cd7387ee
MazeWave <louisdalmasso2003@gmail.com>
2024-11-13 14:48:27 +0100

    cpp7 ex01

---
+++ projects/cpp_heaven/cpp7/ex01/iter.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldalmass <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/13 18:02:06 by ldalmass          #+#    #+#             */
+/*   Updated: 2024/11/12 21:25:11 by ldalmass         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/************* STANDARD LIBRARIES ***************/
+# pragma once
+# include <iostream>
+# include <string>
+# include <exception>
+
+/******************* CLASSES ********************/
+
+template<typename T>
+void	iter(T* array, unsigned long len, void (&foo)(T &))
+{
+	for (unsigned long i = 0; i < len; i++)
+		foo(array[i]);
+}
+
+/****************** FUNCTIONS *******************/
+template<typename T>
+void	print_data(T & data) {std::cout << data << std::endl; return;}
+
+/******************* COLORS *********************/
+# define RESET		"\033[0m"
+# define RED		"\033[31m"
+# define GREEN		"\033[32m"
+# define BLUE		"\033[34m"
+# define CYAN		"\033[36m"
+# define MAGENTA	"\033[35m"
+# define YELLOW		"\033[33m"
+# define BLACK		"\033[30m"
+# define WHITE		"\033[37m"
+
+# define DEBUG 0
