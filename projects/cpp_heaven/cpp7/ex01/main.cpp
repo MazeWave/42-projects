5321bde0ce69d7acc86536779ec15a170a077a96
MazeWave <louisdalmasso2003@gmail.com>
2024-12-11 01:39:04 +0100

    cpp7 should be good to go for repush

--- projects/cpp_heaven/cpp7/ex01/main.cpp
+++ projects/cpp_heaven/cpp7/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldalmass <ldalmass@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ldalmass <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 17:52:13 by ldalmass          #+#    #+#             */
-/*   Updated: 2024/12/10 19:29:47 by ldalmass         ###   ########.fr       */
+/*   Updated: 2024/12/11 01:38:12 by ldalmass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@ int		main(void)
 {
 	int			a[3] = {-42, 0, 42};
 	std::string	b[2] = {"Hello", "World!"};
+	const int	c[2] = {1, 2};
 
 	iter<int>(a, 3, print_data);
 	iter<std::string>(b, 2, print_data);
+	iter<const int>(c, 2, print_data);
+
 	return (0);
 }
 
